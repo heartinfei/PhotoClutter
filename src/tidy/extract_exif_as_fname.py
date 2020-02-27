87806383382639aad5436a0acf17fe812c39de2d
Rango <wangqiang@smzdm.com>
2020-02-27 13:45:49 +0800

    [FEAT] 增加对视频文件的处理

--- src/tidy/extract_exif_as_fname.py
+++ src/tidy/extract_exif_as_fname.py
@@ -4,6 +4,7 @@
 import exifread
 import os
 import datetime
+from moviepy.editor import VideoFileClip
 
 
 def rename_file_in_dir(d):
@@ -20,18 +21,59 @@ def rename_file_in_dir(d):
             rename_file_in_dir(f_name)
             break
         else:
-            img = open(f_name, "rb")
-            tags = exifread.process_file(img)
-            now = datetime.datetime.now()
-            shot_time = tags.get('EXIF DateTimeOriginal', now)
-            make = tags.get('Image Make', "unknow")
-            model = tags.get('Image Model', 'unknow')
-            new_name = "{}_{}_{}.jpeg".format(shot_time, make, model).replace(" ", "_")
-            new_path = os.path.join(d, new_name)
-            try:
-                os.rename(f_name, new_path)
-            except NotImplementedError:
-                print("extract_exif_as_fname-->NotImplementedError")
+            if element.endswith(".jpeg"):
+                # 对照片重命名
+                new_name = os.path.join(d, gen_name_for_img(f_name))
+                rename_file(f_name, new_name)
+            elif element.endswith(".mp4") or element.endswith(".mov"):
+                # 对视频文件重命名
+                new_name = os.path.join(d, gen_name_for_video(f_name))
+                rename_file(f_name, new_name)
+
+
+def rename_file(old_name, new_name):
+    """
+    重命名文件
+    :param old_name:
+    :param new_name:
+    :return:
+    """
+    try:
+        os.rename(old_name, new_name)
+    except NotImplementedError:
+        print("extract_exif_as_fname-->NotImplementedError")
+
+
+def gen_name_for_img(img_full_name: str) -> str:
+    img = open(img_full_name, "rb")
+    tags = exifread.process_file(img)
+    now = datetime.datetime.now()
+    shot_time = tags.get('EXIF DateTimeOriginal', now)
+    make = tags.get('Image Make', "unknow")
+    model = tags.get('Image Model', 'unknow')
+    return "{}_{}_{}.jpeg".format(shot_time, make, model).replace(" ", "_")
+
+
+def gen_name_for_video(video_full_name: str) -> str:
+    clip = VideoFileClip(video_full_name)
+    duration = format_duration(clip.duration)
+    return u'%s%s.mp4' % (clip.filename, duration)
+
+
+def format_duration(video_duration: int) -> str:
+    M, H = 60, 60 ** 2
+    if video_duration < M:
+        return u'%ds' % int(video_duration)
+    else:
+        if video_duration < H:
+            minute = int(video_duration / M)
+            second = int(video_duration % M)
+            return u'%dm%ds' % (minute, second)
+        else:
+            hour = int(video_duration / H)
+            minute = int(video_duration % H / M)
+            second = int(video_duration % H % M)
+        return u'%dh%dm%ds' % (hour, minute, second)
 
 
 if __name__ == "__main__":
--- src/tidy/tidy_up.py
+++ src/tidy/tidy_up.py
@@ -33,22 +33,74 @@ def del_by_list(file_list: list):
             print("tidy_up-->NotImplementedError")
 
 
-def move_to_target_dir(src, dst):
+def flat_move_to_target_dir(src, dst):
+    """
+    将目标文件夹（src）及子文件夹中的所有文件移动到dst文件夹
+    :param src: 源文件夹
+    :param dst: 目标文件夹（存放整理后的内容）
+    :return: None
+    """
     for element in os.listdir(src):
         full_name = os.path.join(src, element)
         if full_name.startswith(".") or full_name.endswith(".photoslibrary"):
             continue
         if os.path.isdir(full_name):
-            move_to_target_dir(full_name, dst)
+            flat_move_to_target_dir(full_name, dst)
         if full_name.endswith(".jpeg"):
             if not os.path.exists(dst):
                 os.mkdir(dst)
             shutil.move(full_name, os.path.join(dst, element))
 
 
-if __name__ == "__main__":
-    target_dir = input("请输入文件目录：")
-    result_list = handle_target_dir(target_dir)
+def search_duplicate_file():
+    result = handle_target_dir(input("请输入文件目录："))
+    log_path = input("请输入结果保存路径：")
+    log_file = open(log_path + ".log", "rw")
+    for r in result:
+        log_file.write(r + "\n")
+    log_file.close()
+    print("搜索结果保存在 {}".format(log_path))
+
+
+def rename_all_file_in_target_dir():
+    rename_file_in_dir(input("请输入文件目录："))
+
+
+def flat_dir():
+    flat_move_to_target_dir(input("请输入待整理文件目录："), input("请输入存放整理结果目录："))
+
+
+def auto_tidy_up():
+    """
+    自动整理文件夹
+    :return:
+    """
+    src_dir = input("请输入文件目录：")
+    dst_dir = input("请输入存放整理结果目录：")
+    result_list = handle_target_dir(src_dir)
     del_by_list(result_list)
-    rename_file_in_dir(target_dir)
-    move_to_target_dir(target_dir, "/Users/smzdm/imgs_new")
+    rename_file_in_dir(src_dir)
+    flat_move_to_target_dir(src_dir, dst_dir)
+
+
+def default():
+    print("No such command.")
+
+
+if __name__ == "__main__":
+    tips = """
+    *********************************
+    * 1. 搜索重复文件
+    * 2. 重命名文件
+    * 3. 文件夹子目录展平
+    * 4. 智能整理文件夹（1 & 2 & 3）
+    *********************************
+    """
+    print(tips)
+    cmd = input("请输入功能索引：")
+    switch = {"1": search_duplicate_file,
+              '2': rename_all_file_in_target_dir,
+              '3': flat_dir,
+              '4': auto_tidy_up}
+
+    switch.get(cmd, default)()
